9298d069601ce17d9436e7867378b646875c7f32
Ruslan Gren <gren.ruslan@gmail.com>
2023-02-28 22:56:54 +0200

    completed the task

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,8 +6,8 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
-
-
+        int h = seconds/3600, mm = seconds/60%60, ss = seconds%60;
+        System.out.printf("%d:%02d:%02d", h%24, mm, ss);
 
     }
 }
